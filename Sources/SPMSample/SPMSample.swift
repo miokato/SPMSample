594dd86fb6721af70b122fb7f236d1cac5c4c8f8
miokato <miokato@me.com>
2024-04-18 10:45:07 +0900

    Add public initializer.

--- Sources/SPMSample/SPMSample.swift
+++ Sources/SPMSample/SPMSample.swift
@@ -6,6 +6,10 @@ import UIKit
 public struct SampleStruct {
     public var value: Int = 0
     
+    public init(value: Int) {
+        self.value = value
+    }
+    
     public func sampleMethod() {
         print("Hello SPM")
     }
