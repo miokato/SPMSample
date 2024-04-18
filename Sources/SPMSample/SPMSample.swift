105a8a8c93585c431f2fd8ed67b46cd33deecdbc
miokato <miokato@me.com>
2024-04-18 10:33:24 +0900

    Add SampleStruct.

--- Sources/SPMSample/SPMSample.swift
+++ Sources/SPMSample/SPMSample.swift
@@ -2,3 +2,11 @@
 // https://docs.swift.org/swift-book
 
 import UIKit    
+
+public struct SampleStruct {
+    var value: Int = 0
+    
+    func sampleMethod() {
+        print("Hello SPM")
+    }
+}
